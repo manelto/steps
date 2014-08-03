8e22e93639fcd5412cb01e16fe5053931e374e37
manelto <maneltorrecilla@infonegocio.com>
2014-08-03 14:37:59 +0200

    Update run.c

--- src/run.c
+++ src/run.c
@@ -101,8 +101,8 @@ void inicio(void)
   
   //parte accelerometer
 
-  text_layer_1 = text_layer_create(GRect(0, 0, 144, 18));
-  text_layer_2 = text_layer_create(GRect(0, 20, 144, 18));
+  text_layer_1 = text_layer_create(GRect(0, 0, frame.size.w, 22));
+  text_layer_2 = text_layer_create(GRect(0, 22, frame.size.w, 18));
   text_layer_set_text_color(text_layer_1, GColorWhite);
   text_layer_set_background_color(text_layer_1, GColorClear);
   text_layer_set_text_color(text_layer_2, GColorWhite);
@@ -111,7 +111,7 @@ void inicio(void)
   layer_add_child(root_layer, text_layer_get_layer(text_layer_2));
  
   accel_data_service_subscribe(1, accel_handler);
-  accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
  
   accel_tap_service_subscribe(tap_handler);
   //----------------------
